046d6d35f0add90302219b95bc76d181a280c3fa
Elizabeth Siegle <lizzie.siegle@gmail.com>
2017-10-08 12:28:31 -0400

    Initial Commit

---
+++ ClarifaiCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClarifaiCam
+//
+//  Created by Elizabeth Siegle on 10/8/17.
+//  Copyright © 2017 Elizabeth Siegle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
